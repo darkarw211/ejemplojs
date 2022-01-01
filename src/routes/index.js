92a1c04ad38a5c88d893e8f687da1ffa6dbf4344
vidaña <rangel_2211@hotmail.com>
2021-12-31 20:24:36 -0600

    first commit

---
+++ src/routes/index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/',(req,res)=>{
+    res.render('index.ejs',{
+        nombre:'Sergio'
+    })
+});
+
+router.get('/saludo',(req,res)=>{
+    res.send('Hola, este es el saludo desde express para recuperar la materia T-T')
+});
+
+router.get('/login',(req,res)=>{
+    res.render('login.ejs')
+});
+
+router.get('/studiambre',(req,res)=>{
+    res.render('studiambre.ejs')
+});
+
+
+module.exports = router;
